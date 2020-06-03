4ece8c048d8270769c3e8f26e1b9c330aa3244a5
parthparikh <parthparikh1999p@gmail.com>
2020-06-04 01:38:25 +0530

    Added method (get_and_scale_emobank) to scale Emobank's 1-5 range to 1-9

--- src/categories.py
+++ src/categories.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from scipy.stats import truncnorm
+from sklearn.neighbors import KNeighborsClassifier
+import regression_paper
 
 # To see how this mapping was derived, see - 
 # Affect representation and recognition in 3D continuous valence–arousal–dominance space
@@ -11,7 +13,19 @@ EMOTION_VAD_MAP_PATH = "../lemma/emotions.csv"
 
 class Categorization:
     def __init__(self):
-        pass
+        self.reg_paper_obj = regression_paper.Gutenberg_Emotion()
+
+    def get_and_scale_emobank(self, filename=regression_paper.EMOBANK_PATH):
+        df = self.reg_paper_obj.get_corpus(filename)
+        emo_min, emo_max, map_min, map_max = 1, 5, 1, 9
+        old_range = (emo_max - emo_min)
+        new_range = (map_max - map_min)
+
+        df["V"] = df["V"].apply(lambda x: (((x - emo_min) * new_range) / old_range) + map_min)
+        df["A"] = df["A"].apply(lambda x: (((x - emo_min) * new_range) / old_range) + map_min)
+        df["D"] = df["D"].apply(lambda x: (((x - emo_min) * new_range) / old_range) + map_min)
+
+        return df
 
     def get_mapping(self, filename=EMOTION_VAD_MAP_PATH):
         return pd.read_csv(open(EMOTION_VAD_MAP_PATH))
@@ -23,7 +37,7 @@ class Categorization:
 
     def generate_random_category(self, category, V_mean, V_std, 
                                     A_mean, A_std, D_mean, 
-                                    D_std, length=5):
+                                    D_std, length=100000):
         V = self.get_truncated_normal(V_mean, V_std, 
                                     V_mean - V_std, V_mean + V_std, length)
         A = self.get_truncated_normal(A_mean, A_std, 
@@ -47,6 +61,15 @@ class Categorization:
 
         return (combined_X, combined_y)
 
+    def train(self, X, y, n_neighbors, weights):
+        neigh = KNeighborsClassifier(n_neighbors=n_neighbors, weights=weights)
+        neigh.fit(X, y)
+        return neigh
+
 if __name__ == "__main__":
     obj = Categorization()
-    print(obj.training_set(obj.get_mapping()))
+    X, y = obj.training_set(obj.get_mapping())
+    neigh = obj.train(X, y, 5, 'uniform')
+    print(neigh.classes_)
+    print(neigh.predict_proba([(5.88, 5, 5.44)]))
+    print(neigh.predict([(5.88, 5, 5.44)]))
