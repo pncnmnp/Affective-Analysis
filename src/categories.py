1162a99b9c4b63d8e09996aafc25f3c4a2b7c5ef
parthparikh <parthparikh1999p@gmail.com>
2020-07-20 09:45:16 +0530

    Added methods to create Random Forest and Adaboost models Updated K Nearest Neighbours Classifier with optimal hyperparameters obtained after tuning Updated __main__ to generate and store emotions of all the books from Gutenberg corpus

--- src/categories.py
+++ src/categories.py
@@ -1,9 +1,14 @@
 from scipy.stats import truncnorm
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neighbors import KNeighborsClassifier, RadiusNeighborsClassifier
 from collections import Counter
+
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 
 import pandas as pd
 import regression_paper
+import pickle
+import json
+import os.path
 
 # To see how this mapping was derived, see - 
 # Affect representation and recognition in 3D continuous valence–arousal–dominance space
@@ -17,28 +22,30 @@ class Categorization:
     def __init__(self):
         self.reg_paper_obj = regression_paper.Gutenberg_Emotion()
 
-    def get_and_scale_emobank(self, filename=regression_paper.EMOBANK_PATH):
+    def get_and_scale_emobank(self, map_min=1, map_max=9, filename=regression_paper.EMOBANK_PATH):
         """
         Returns Emobank corpus as a Pandas Dataframe.
         The V,A,D values in Emobank corpus are scaled from 1-5 to 1-9.
 
         Params: filename - Path of the emobank corpus
                            (Default - EMOBANK_PATH in regression_paper.py)
+                map_min, map_max - 1,9 for Warriner
+                                   0,1 for NRC
 
         Returns: df - Pandas Dataframe containing Emobank corpus
         """
         df = self.reg_paper_obj.get_corpus(filename)
-        emo_min, emo_max, map_min, map_max = 1, 5, 1, 9
+        emo_min, emo_max = 1, 5
         old_range = (emo_max - emo_min)
         new_range = (map_max - map_min)
 
-        df["V"] = df["V"].apply(lambda x: (((x - emo_min) * new_range) / old_range) + map_min)
-        df["A"] = df["A"].apply(lambda x: (((x - emo_min) * new_range) / old_range) + map_min)
-        df["D"] = df["D"].apply(lambda x: (((x - emo_min) * new_range) / old_range) + map_min)
+        df["valence"] = df["valence"].apply(lambda x: (((x - emo_min) * new_range) / old_range) + map_min)
+        df["arousal"] = df["arousal"].apply(lambda x: (((x - emo_min) * new_range) / old_range) + map_min)
+        df["dominance"] = df["dominance"].apply(lambda x: (((x - emo_min) * new_range) / old_range) + map_min)
 
         return df
 
-    def map_emobank(self, models=10, train_length=10000, to_save=False, filename="model.csv"):
+    def map_emobank(self, book_id, models=10, train_length=10000, to_save=False, filename="model.csv"):
         """
         Predict the emotions present in each text in Emobank corpus.
         Returns Emobank corpus as a Pandas Dataframe.
@@ -77,24 +84,68 @@ class Categorization:
         """
         # Training X different models (X = models)
         trained_models = list()
+
+        progress = 0.01
         for index in range(models):
+            # Debug info
+            if (index/models) > progress:
+                print("Training models: {}%\r".format(round(progress*100), 2), end="")
+                progress += 0.01
+
             X, y = self.training_set(df=self.get_mapping(), length=train_length)
-            neigh = self.train(X, y, 5, 'uniform')
+            # neigh = self.train(X, y, 12, 'distance')
+            neigh = self.train_random_forest(X, y)
+            # neigh = self.train_adaboost(X, y)
             trained_models.append(neigh)
 
-        df = self.get_and_scale_emobank()
+        with open('random_forrest_15_4_10.pkl', 'wb') as f:
+            pickle.dump(trained_models, f)
+
+        # with open('random_forrest_15_4_10.pkl', 'rb') as f:
+        #     trained_models = pickle.load(f)
+
+        # df = self.get_and_scale_emobank(filename="1777.csv")
+        df = pd.read_csv(open("../books/stats/"+book_id+".csv"))
+
+        # Removing values in space 4.5 - 5.5 (so called neutral range)
+        vm = df.describe()["valence"]["mean"]
+        am = df.describe()["arousal"]["mean"]
+        dm = df.describe()["dominance"]["mean"]
+        vs = df.describe()["valence"]["std"]
+        ars = df.describe()["arousal"]["std"]
+        ds = df.describe()["dominance"]["std"]
+
+        df = df[((df["valence"] < (vm-vs)) | (df["valence"] > (vm+vs))) | 
+                ((df["arousal"] < (am-ars)) | (df["arousal"] > (am+ars))) | 
+                ((df["dominance"] < (dm-ds)) | (df["dominance"] > (dm+ds)))]
+
+        # df = df[((df["valence"] < (4.5)) | (df["valence"] > (5.5))) | 
+        #         ((df["arousal"] < (4.5)) | (df["arousal"] > (5.5))) | 
+        #         ((df["dominance"] < (4.5)) | (df["dominance"] > (5.5)))]
+
         # Add a column 'emotions' with None values
         df["emotions"] = pd.Series("", index=df.index)
 
+        progress = 0.01
         for index in range(len(df)):
+            # Debug info
+            if (index/len(df)) > progress:
+                print("Progress: {}%\r".format(round(progress*100), 2), end="")
+                progress += 0.01
+
             emotion = list()
             for e in range(models):
-                vals = (df.iloc[index]["V"], df.iloc[index]["A"], df.iloc[index]["D"])
-                emotion += list(trained_models[e].predict([vals]))
-            df.at[index, "emotions"] = dict(Counter(emotion))
+                if pd.isnull(df.iloc[index]["valence"]) == False:
+                    vals = (df.iloc[index]["valence"], df.iloc[index]["arousal"], df.iloc[index]["dominance"])
+                    try:
+                        emotion += list(trained_models[e].predict([vals]))
+                    except:
+                        emotion += [None]
+            name = df.iloc[index].name
+            df.at[name, "emotions"] = dict(Counter(emotion))
 
         if to_save == True:
-            df.to_csv(filename, encoding='utf-8', index=False)
+            df.to_csv(filename, encoding='utf-8', index=True, index_label="book_index")
         else:
             return df
 
@@ -113,7 +164,7 @@ class Categorization:
         
         Returns: Pandas Dataframe of the mapping
         """
-        return pd.read_csv(open(EMOTION_VAD_MAP_PATH))
+        return pd.read_csv(open(filename))
 
     def get_truncated_normal(self, mean, sd, low, up, length):
         """
@@ -207,13 +258,43 @@ class Categorization:
 
         Returns: Trained KNN model
         """
-        neigh = KNeighborsClassifier(n_neighbors=n_neighbors, weights=weights)
+        neigh = KNeighborsClassifier(n_neighbors=n_neighbors, weights=weights, algorithm="ball_tree")
+        # neigh = RadiusNeighborsClassifier(radius=0.1)
         neigh.fit(X, y)
         return neigh
 
+    def train_random_forest(self, X, y):
+        """
+        Trains a Random Forest model.
+        """
+        clf = RandomForestClassifier(max_depth=15, min_samples_leaf=4, min_samples_split=10, oob_score=True, random_state=0)
+        clf.fit(X, y)
+        return clf
+
+    def train_adaboost(self, X, y):
+        """
+        Trains a Adaboost model.
+        """
+        clf = AdaBoostClassifier(n_estimators=200, random_state=0, algorithm="SAMME")
+        clf.fit(X, y)
+        return clf
+
 if __name__ == "__main__":
+    GUTENBERG_ID_PATH = "../books/gutenberg_book_ids.json"
+    ids = json.load(open(GUTENBERG_ID_PATH))
     obj = Categorization()
-    obj.map_emobank(models=100, train_length=1000000, to_save=True)
+
+    for book_id in ids:
+        filepath = "../books/predicted/" + str(book_id) + ".csv"
+        if os.path.isfile(filepath) == True:
+            print("Already Scanned: {}".format(book_id))
+            continue
+
+        try:
+            print("Scanning book: {}".format(book_id))
+            obj.map_emobank(str(book_id), models=100, train_length=100000, to_save=True, filename=filepath)
+        except:
+            print("FAILED: {}".format(book_id))
     # X, y = obj.training_set(obj.get_mapping())
     # neigh = obj.train(X, y, 5, 'uniform')
     # print(neigh.classes_)
